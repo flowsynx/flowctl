1151e2dd6440574ab41b6d7d7eaccba9c820d1d5
ziagham <amin.ziagham@gmail.com>
2023-12-29 00:34:45 +0100

    Delete unneccessities extensions and add some new extensions

--- src/FlowSynx.Cli/Extensions/ServiceCollectionExtensions.cs
+++ src/FlowSynx.Cli/Extensions/ServiceCollectionExtensions.cs
@@ -5,9 +5,16 @@ using FlowSynx.Cli.Commands.Execute;
 using Spectre.Console;
 using FlowSynx.Cli.Commands;
 using FlowSynx.Cli.ApplicationBuilders;
+using FlowSynx.Cli.Commands.Config;
+using FlowSynx.Cli.Formatter;
 using FlowSynx.Environment;
 using FlowSynx.IO;
 using FlowSynx.Net;
+using FlowSynx.Logging;
+using Microsoft.Extensions.Logging;
+using FlowSynx.Cli.Commands.Plugins;
+using FlowSynx.Cli.Commands.Version;
+using FlowSynx.Cli.Commands.Health;
 
 namespace FlowSynx.Cli.Extensions;
 
@@ -18,6 +25,10 @@ public static class ServiceCollectionExtensions
         services.AddTransient<RootCommand, Root>();
         services.AddTransient<Command, RunCommand>();
         services.AddTransient<Command, StorageCommand>();
+        services.AddTransient<Command, ConfigCommand>();
+        services.AddTransient<Command, PluginsCommand>();
+        services.AddTransient<Command, VersionCommand>();
+        services.AddTransient<Command, HealthCommand>();
         return services;
     }
 
@@ -29,6 +40,31 @@ public static class ServiceCollectionExtensions
         services.AddHttpRequestService();
         services.AddSingleton(AnsiConsole.Console);
         services.AddTransient<ICliApplicationBuilder, CliApplicationBuilder>();
+        return services;
+    }
+
+    public static IServiceCollection AddLoggingService(this IServiceCollection services)
+    {
+        services.AddLogging(c => c.ClearProviders());
+        const string template = "[time={timestamp} | level={level} | machine={machine}] message=\"{message}\"";
+        services.AddLogging(builder => builder.AddConsoleLogger(config =>
+        {
+            config.OutputTemplate = template;
+            config.MinLevel = LogLevel.None;
+        }));
+        return services;
+    }
+
+    public static IServiceCollection AddFormatter(this IServiceCollection services)
+    {
+        services.AddTransient<ISpinner, Formatter.Spinner>();
+        services.AddTransient<IOutputFormatter, OutputFormatter>();
+        return services;
+    }
+
+    public static IServiceCollection AddVersion(this IServiceCollection services)
+    {
+        services.AddTransient<IVersion, CliVersion>();
         return services;
     }
 }
