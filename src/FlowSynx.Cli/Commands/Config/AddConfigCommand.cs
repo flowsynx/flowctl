17ba73abf29d7fdd66e2a4e88000b780e6926d59
ziagham <amin.ziagham@gmail.com>
2024-01-02 21:22:43 +0100

    Add functionality to parse and support adding specifications adding new configuration item

--- src/FlowSynx.Cli/Commands/Config/AddConfigCommand.cs
+++ src/FlowSynx.Cli/Commands/Config/AddConfigCommand.cs
@@ -5,6 +5,7 @@ using FlowSynx.IO.Serialization;
 using FlowSynx.Net;
 using EnsureThat;
 using FlowSynx.Cli.Formatter;
+using FlowSynx.IO.Exceptions;
 
 namespace FlowSynx.Cli.Commands.Config;
 
@@ -14,9 +15,11 @@ internal class AddConfigCommand : BaseCommand<AddConfigCommandOptions, AddConfig
     {
         var nameOption = new Option<string>(new[] { "--name" }, "The path to get about") { IsRequired = true };
         var typeOption = new Option<string>(new[] { "--type" }, "The path to get about") { IsRequired = true };
+        var specificationsOption = new Option<string>(new[] { "--spec" }, "The path to get about");
 
         AddOption(nameOption);
         AddOption(typeOption);
+        AddOption(specificationsOption);
     }
 }
 
@@ -24,6 +27,7 @@ internal class AddConfigCommandOptions : ICommandOptions
 {
     public string Name { get; set; } = string.Empty;
     public string Type { get; set; } = string.Empty;
+    public string Spec { get; set; } = string.Empty;
 }
 
 internal class AddConfigCommandOptionsHandler : ICommandOptionsHandler<AddConfigCommandOptions>
@@ -32,9 +36,10 @@ internal class AddConfigCommandOptionsHandler : ICommandOptionsHandler<AddConfig
     private readonly ISpinner _spinner;
     private readonly IEndpoint _endpoint;
     private readonly IHttpRequestService _httpRequestService;
+    private readonly IDeserializer _deserializer;
 
     public AddConfigCommandOptionsHandler(IOutputFormatter outputFormatter, ISpinner spinner,
-        IEndpoint endpoint, IHttpRequestService httpRequestService, ISerializer serializer)
+        IEndpoint endpoint, IHttpRequestService httpRequestService, ISerializer serializer, IDeserializer deserializer)
     {
         EnsureArg.IsNotNull(outputFormatter, nameof(outputFormatter));
         EnsureArg.IsNotNull(endpoint, nameof(endpoint));
@@ -44,6 +49,7 @@ internal class AddConfigCommandOptionsHandler : ICommandOptionsHandler<AddConfig
         _spinner = spinner;
         _endpoint = endpoint;
         _httpRequestService = httpRequestService;
+        _deserializer = deserializer;
     }
 
     public async Task<int> HandleAsync(AddConfigCommandOptions options, CancellationToken cancellationToken)
@@ -57,13 +63,24 @@ internal class AddConfigCommandOptionsHandler : ICommandOptionsHandler<AddConfig
         try
         {
             const string relativeUrl = "config/add";
-            var request = new AddConfigRequest { Name = options.Name, Type = options.Type };
+
+            var specification = new Dictionary<string, string?>();
+            if (!string.IsNullOrEmpty(options.Spec))
+            {
+                specification = _deserializer.Deserialize<Dictionary<string, string?>>(options.Spec);
+            }
+
+            var request = new AddConfigRequest {Name = options.Name, Type = options.Type, Specifications = specification};
             var result = await _httpRequestService.PostAsync<AddConfigRequest, Result<AddConfigResponse?>>($"{_endpoint.GetDefaultHttpEndpoint()}/{relativeUrl}", request, cancellationToken);
 
             if (!result.Succeeded)
                 _outputFormatter.WriteError(result.Messages);
             else
                 _outputFormatter.Write(result.Data);
+        }
+        catch (DeserializerException)
+        {
+            _outputFormatter.WriteError("Could not parse the entered specifications. Please check it and try again!");
         }
         catch (Exception ex)
         {
@@ -76,7 +93,7 @@ public class AddConfigRequest
 {
     public required string Name { get; set; }
     public required string Type { get; set; }
-    public object? Specifications { get; set; }
+    public Dictionary<string, string?>? Specifications { get; set; }
 }
 
 public class AddConfigResponse
