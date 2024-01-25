c60bdaff62fd7f02b0e7385ab0110ee2a5b74444
ziagham <amin.ziagham@gmail.com>
2024-01-25 21:02:24 +0100

    Refactoring code in a such way that can remove flowsynx engine and synx cli itself.

--- src/FlowSynx.Cli/Commands/Execute/UninstallCommand.cs
+++ src/FlowSynx.Cli/Commands/Execute/UninstallCommand.cs
@@ -1,10 +1,9 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.CommandLine;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using EnsureThat;
 using FlowSynx.Cli.Formatter;
 using FlowSynx.Cli.Services;
-using FlowSynx.Environment;
-using FlowSynx.IO.Compression;
-using FlowSynx.IO.Serialization;
 
 namespace FlowSynx.Cli.Commands.Execute;
 
@@ -12,69 +11,144 @@ internal class UninstallCommand : BaseCommand<UninstallCommandOptions, Uninstall
 {
     public UninstallCommand() : base("uninstall", "Uninstalling FlowSynx engine and cli")
     {
+        var forceOption = new Option<bool>(new[] { "--force" }, getDefaultValue :() => false, description: "Force terminate FlowSynx engine if it is running");
+
+        AddOption(forceOption);
     }
 }
 
 internal class UninstallCommandOptions : ICommandOptions
 {
-
+    public bool Force { get; set; }
 }
 
 internal class UninstallCommandOptionsHandler : ICommandOptionsHandler<UninstallCommandOptions>
 {
     private readonly IOutputFormatter _outputFormatter;
     private readonly ISpinner _spinner;
-    private readonly IVersion _version;
     private readonly ILocation _location;
-    private readonly IOperatingSystemInfo _operatingSystemInfo;
-    private readonly IZipFile _zipFile;
-    private readonly IGZipFile _gZipFile;
-    private readonly IDeserializer _deserializer;
 
-    public UninstallCommandOptionsHandler(IOutputFormatter outputFormatter, ISpinner spinner,
-        IVersion version, ILocation location, IOperatingSystemInfo operatingSystemInfo,
-        IZipFile zipFile, IGZipFile gZipFile, IDeserializer deserializer)
+    public UninstallCommandOptionsHandler(IOutputFormatter outputFormatter, ISpinner spinner, ILocation location)
     {
         EnsureArg.IsNotNull(outputFormatter, nameof(outputFormatter));
         EnsureArg.IsNotNull(spinner, nameof(spinner));
-        EnsureArg.IsNotNull(version, nameof(version));
         EnsureArg.IsNotNull(location, nameof(location));
-        EnsureArg.IsNotNull(operatingSystemInfo, nameof(operatingSystemInfo));
-        EnsureArg.IsNotNull(zipFile, nameof(zipFile));
-        EnsureArg.IsNotNull(gZipFile, nameof(gZipFile));
-        EnsureArg.IsNotNull(deserializer, nameof(deserializer));
 
         _outputFormatter = outputFormatter;
         _spinner = spinner;
-        _version = version;
         _location = location;
-        _operatingSystemInfo = operatingSystemInfo;
-        _zipFile = zipFile;
-        _gZipFile = gZipFile;
-        _deserializer = deserializer;
     }
+
+    private string UserProfilePath => System.Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile);
+    private string DefaultFlowSynxDirName => Path.Combine(UserProfilePath, ".flowsynx");
 
     public async Task<int> HandleAsync(UninstallCommandOptions options, CancellationToken cancellationToken)
     {
-        await _spinner.DisplayLineSpinnerAsync(async () => await Execute(options, cancellationToken));
+        await _spinner.DisplayLineSpinnerAsync(() => Execute(options, cancellationToken));
         return 0;
     }
 
-    private async Task Execute(UninstallCommandOptions options, CancellationToken cancellationToken)
+    private Task Execute(UninstallCommandOptions options, CancellationToken cancellationToken)
     {
         try
         {
             _outputFormatter.Write("Beginning uninstalling...");
+
+            if (options.Force)
+            {
+                TerminateProcess("FlowSynx", ".");
+            }
+            else
+            {
+                if (IsProcessRunning("FlowSynx", "."))
+                {
+                    _outputFormatter.Write("The FlowSynx engine is running. Please stop it by run the command: 'Synx stop', and try uninstall again.");
+                    return Task.CompletedTask;
+                }
+            }
+
+            FlowSynxDestruction();
+            SelfDestruction();
+            _outputFormatter.Write("Uninstalling is done!");
         }
         catch (Exception e)
         {
             _outputFormatter.WriteError(e.Message);
         }
+        return Task.CompletedTask;
+    }
+
+    private bool IsProcessRunning(string processName, string machineAddress)
+    {
+        var processes = Process.GetProcessesByName(processName, machineAddress);
+        return processes.Length != 0;
+    }
+
+    private void TerminateProcess(string processName, string machineAddress)
+    {
+        var processes = Process.GetProcessesByName(processName, machineAddress);
+
+        if (processes.Length == 0) return;
+
+        try
+        {
+            foreach (var process in processes)
+                process.Kill();
+
+            _outputFormatter.Write("The FlowSynx engine was stopped successfully.");
+        }
+        catch (Exception ex)
+        {
+            _outputFormatter.WriteError(ex.Message);
+        }
+    }
+
+    private void FlowSynxDestruction()
+    {
+        if (Directory.Exists(DefaultFlowSynxDirName))
+            Directory.Delete(DefaultFlowSynxDirName, true);
+    }
+
+    private void SelfDestruction()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var procDestruct = new Process();
+            var strName = "destruct.bat";
+            var strPath = Path.Combine(Directory.GetCurrentDirectory(), strName);
+            var strExe = new FileInfo(LookupBinaryFilePath(_location.RootLocation)).Name;
+
+            var swDestruct = new StreamWriter(strPath);
+            swDestruct.WriteLine("attrib \"" + strExe + "\"" + " -a -s -r -h");
+            swDestruct.WriteLine(":Repeat");
+            swDestruct.WriteLine("del " + "\"" + strExe + "\"");
+            swDestruct.WriteLine("if exist \"" + strExe + "\"" + " goto Repeat");
+            swDestruct.WriteLine("del \"" + strName + "\"");
+            swDestruct.Close();
+
+            procDestruct.StartInfo.FileName = "destruct.bat";
+            procDestruct.StartInfo.CreateNoWindow = true;
+            procDestruct.StartInfo.UseShellExecute = false;
+
+            try
+            {
+                procDestruct.Start();
+            }
+            catch (Exception)
+            {
+                System.Environment.Exit(0);
+            }
+        }
+        else
+        {
+            var strExe = new FileInfo(LookupBinaryFilePath(_location.RootLocation)).FullName;
+            File.Delete(strExe);
+        }
     }
     
     private string LookupBinaryFilePath(string path)
     {
-        var binFileName = "FlowSynx";
+        var binFileName = "Synx";
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             binFileName += ".exe";
 
