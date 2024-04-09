3e9e48b7ad2c3fd09cc9ea2b897d01b452e885d6
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-04-09 23:33:13 +0200

    split the classes into seperate files

--- src/FlowSynx.Cli/Commands/Storage/About/AboutCommand.cs
+++ src/FlowSynx.Cli/Commands/Storage/About/AboutCommand.cs
@@ -1,9 +1,4 @@
 ﻿using System.CommandLine;
-using FlowSynx.Abstractions;
-using FlowSynx.Environment;
-using FlowSynx.Net;
-using EnsureThat;
-using FlowSynx.Cli.Formatter;
 
 namespace FlowSynx.Cli.Commands.Storage.About;
 
@@ -18,71 +13,5 @@ internal class AboutCommand : BaseCommand<AboutCommandOptions, AboutCommandOptio
         AddOption(pathOption);
         AddOption(fullOption);
         AddOption(outputFormatOption);
-    }
-}
-
-internal class AboutCommandOptions : ICommandOptions
-{
-    public string? Path { get; set; } = string.Empty;
-    public bool? Full { get; set; } = false;
-    public Output Output { get; set; } = Output.Json;
-}
-
-internal class AboutCommandOptionsHandler : ICommandOptionsHandler<AboutCommandOptions>
-{
-    private readonly IOutputFormatter _outputFormatter;
-    private readonly ISpinner _spinner;
-    private readonly IEndpoint _endpoint;
-    private readonly IHttpRequestService _httpRequestService;
-
-    public AboutCommandOptionsHandler(IOutputFormatter outputFormatter, ISpinner spinner,
-        IEndpoint endpoint, IHttpRequestService httpRequestService)
-    {
-        EnsureArg.IsNotNull(outputFormatter, nameof(outputFormatter));
-        EnsureArg.IsNotNull(spinner, nameof(spinner));
-        EnsureArg.IsNotNull(endpoint, nameof(endpoint));
-        EnsureArg.IsNotNull(httpRequestService, nameof(httpRequestService));
-
-        _outputFormatter = outputFormatter;
-        _spinner = spinner;
-        _endpoint = endpoint;
-        _httpRequestService = httpRequestService;
-    }
-
-    public async Task<int> HandleAsync(AboutCommandOptions options, CancellationToken cancellationToken)
-    {
-        await _spinner.DisplayLineSpinnerAsync(async () => await Execute(options, cancellationToken));
-        return 0;
-    }
-
-    private async Task Execute(AboutCommandOptions options, CancellationToken cancellationToken)
-    {
-        try
-        {
-            const string relativeUrl = "storage/about";
-            var request = new AboutRequest()
-            {
-                Path = options.Path,
-                Full = options.Full
-            };
-
-            var result = await _httpRequestService.PostRequestAsync<AboutRequest, Result<AboutResponse?>>($"{_endpoint.GetDefaultHttpEndpoint()}/{relativeUrl}", request, cancellationToken);
-
-            if (result is { Succeeded: false })
-            {
-                _outputFormatter.WriteError(result.Messages);
-            }
-            else
-            {
-                if (result?.Data is not null)
-                    _outputFormatter.Write(result.Data, options.Output);
-                else
-                    _outputFormatter.Write(result?.Messages);
-            }
-        }
-        catch (Exception ex)
-        {
-            _outputFormatter.WriteError(ex.Message);
-        }
     }
 }
---
+++ src/FlowSynx.Cli/Commands/Storage/About/AboutCommandOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace FlowSynx.Cli.Commands.Storage.About;
+
+internal class AboutCommandOptions : ICommandOptions
+{
+    public string? Path { get; set; } = string.Empty;
+    public bool? Full { get; set; } = false;
+    public Output Output { get; set; } = Output.Json;
+}
