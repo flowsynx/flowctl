0f86370a4262de5766b8e3886f6250c686ce1fcc
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-04-29 05:01:18 +0200

    add some new function related to PathHelper

--- src/FlowSynx.Cli/Common/PathHelper.cs
+++ src/FlowSynx.Cli/Common/PathHelper.cs
@@ -6,6 +6,7 @@ internal static class PathHelper
 {
     public static string UserProfilePath => System.Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile);
     public static string DefaultFlowSynxDirectoryName => Path.Combine(UserProfilePath, ".flowsynx");
+    public static string DefaultFlowSynxBinaryDirectoryName => Path.Combine(DefaultFlowSynxDirectoryName, "bin");
 
     public static string LookupSynxBinaryFilePath(string path)
     {
@@ -25,6 +26,33 @@ internal static class PathHelper
         return Path.Combine(path, binFileName);
     }
 
+    public static string LookupDashboardBinaryFilePath(string path)
+    {
+        var binFileName = "dashboard";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            binFileName += ".exe";
+
+        return Path.Combine(path, binFileName);
+    }
+
+    public static string GetUpdateFilePath()
+    {
+        var binFileName = "Update";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            binFileName += ".exe";
+
+        return binFileName;
+    }
+
+    public static string GetScriptFilePath()
+    {
+        var scriptFileName = "Update.sh";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            scriptFileName = "Update.bat";
+
+        return scriptFileName;
+    }
+
     public static void MakeExecutable(string path)
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return;
@@ -34,5 +62,24 @@ internal static class PathHelper
                                                   UnixFileMode.OtherRead | UnixFileMode.OtherWrite | UnixFileMode.OtherExecute;
 
         File.SetUnixFileMode(path, ownershipPermissions);
+    }
+
+    public static void CopyFilesRecursively(string sourcePath, string targetPath, CancellationToken cancellationToken)
+    {
+        foreach (var dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+        {
+            if (cancellationToken.IsCancellationRequested)
+                break;
+
+            Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+        }
+
+        foreach (var newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+        {
+            if (cancellationToken.IsCancellationRequested)
+                break;
+
+            File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+        }
     }
 }
