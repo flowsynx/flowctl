f36b3cec6a1e499d299c587cde3cc160baac00c2
ziagham <amin.ziagham@outlook.com>
2024-07-07 21:17:28 +0200

    Rename all related namespaces from FlowSynx.Cli to FlowCtl #23

--- src/FlowSynx.Cli/Services/Concretes/OutputFormatter.cs
+++ src/FlowSynx.Cli/Services/Concretes/OutputFormatter.cs
@@ -5,9 +5,9 @@ using Newtonsoft.Json;
 using System.Xml.Linq;
 using FlowSynx.IO.Serialization;
 using FlowSynx.Reflections;
-using FlowSynx.Cli.Services.Abstracts;
+using FlowCtl.Services.Abstracts;
 
-namespace FlowSynx.Cli.Services.Concretes;
+namespace FlowCtl.Services.Concretes;
 
 public class OutputFormatter : IOutputFormatter
 {
