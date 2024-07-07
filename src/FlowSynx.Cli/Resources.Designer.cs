9d15aa1117beb9be081b7b6a30ad3a8ec3c58a5a
ziagham <amin.ziagham@outlook.com>
2024-07-07 21:16:39 +0200

    Rename all related namespaces from FlowSynx.Cli to FlowCtl #23

--- src/FlowSynx.Cli/Resources.Designer.cs
+++ src/FlowSynx.Cli/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FlowSynx.Cli {
+namespace FlowCtl {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FlowSynx.Cli {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FlowSynx.Cli.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FlowCtl.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
