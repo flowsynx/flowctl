4216ede41d94309b1436e3df56931c020a7b54ea
Amin Ziagham <amin.ziagham@outlook.com>
2025-10-28 07:46:13 +0100

    Use '[[' instead of '[' for conditional tests. The '[[' construct is safer and more feature-rich. #115

--- install/install.sh
+++ install/install.sh
@@ -29,7 +29,7 @@ getSystemInfo() {
     OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
 
     # Most linux distro needs root permission to copy the file to /usr/local/bin
-    if [[ "$OS" == "linux" || "$OS" == "darwin" ]] && [ "FLOWCTL_INSTALL_DIR" == "/usr/local/bin" ]; then
+    if [[ "$OS" == "linux" || "$OS" == "darwin" ]] && [[ "FLOWCTL_INSTALL_DIR" == "/usr/local/bin" ]]; then
         USE_SUDO="true"
     fi
 }
@@ -40,13 +40,13 @@ verifySupported() {
     local current_osarch="${OS}-${ARCH}"
 
     for osarch in "${supported[@]}"; do
-        if [ "$osarch" == "$current_osarch" ]; then
+        if [[ "$osarch" == "$current_osarch" ]]; then
             echo "Your system is ${OS}_${ARCH}"
             return
         fi
     done
 
-    if [ "$current_osarch" == "darwin-arm64" ]; then
+    if [[ "$current_osarch" == "darwin-arm64" ]]; then
         if isReleaseAvailable $releaseTag; then
             return
         else
@@ -64,7 +64,7 @@ verifySupported() {
 runAsRoot() {
     local CMD="$*"
 
-    if [ $EUID -ne 0 -a $USE_SUDO = "true" ]; then
+    if [[ $EUID -ne 0 -a $USE_SUDO = "true" ]]; then
         CMD="sudo $CMD"
     fi
 
@@ -86,7 +86,7 @@ checkHttpRequestFlowCtl() {
 }
 
 checkExistingFlowCtl() {
-    if [ -f "$FLOWCTL_FILE" ]; then
+    if [[ -f "$FLOWCTL_FILE" ]]; then
         echo -e "\nFlowCtl is detected:"
         $FLOWCTL_FILE version
         echo -e "Reinstalling FlowCtl - ${FLOWCTL_FILE}...\n"
@@ -99,7 +99,7 @@ getLatestRelease() {
     local flowctlReleaseUrl="https://api.github.com/repos/${GITHUB_ORG}/${GITHUB_REPO}/releases"
     local latest_release=""
 
-    if [ "$HTTP_REQUEST_FLOWCTL" == "curl" ]; then
+    if [[ "$HTTP_REQUEST_FLOWCTL" == "curl" ]]; then
         latest_release=$(curl -s $flowctlReleaseUrl | grep \"tag_name\" | grep -v rc | awk 'NR==1{print $2}' |  sed -n 's/\"\(.*\)\",/\1/p')
     else
         latest_release=$(wget -q --header="Accept: application/json" -O - $flowctlReleaseUrl | grep \"tag_name\" | grep -v rc | awk 'NR==1{print $2}' |  sed -n 's/\"\(.*\)\",/\1/p')
@@ -120,13 +120,13 @@ downloadFile() {
     ARTIFACT_TMP_FILE="$FLOWCTL_TMP_ROOT/$FLOWCTL_ARTIFACT"
 
     echo "Downloading $DOWNLOAD_URL ..."
-    if [ "$HTTP_REQUEST_FLOWCTL" == "curl" ]; then
+    if [[ "$HTTP_REQUEST_FLOWCTL" == "curl" ]]; then
         curl -SsL "$DOWNLOAD_URL" -o "$ARTIFACT_TMP_FILE"
     else
         wget -q -O "$ARTIFACT_TMP_FILE" "$DOWNLOAD_URL"
     fi
 
-    if [ ! -f "$ARTIFACT_TMP_FILE" ]; then
+    if [[ ! -f "$ARTIFACT_TMP_FILE" ]]; then
         echo "failed to download $DOWNLOAD_URL ..."
         exit 1
     fi
@@ -139,15 +139,15 @@ isReleaseAvailable() {
     DOWNLOAD_BASE="https://github.com/${GITHUB_ORG}/${GITHUB_REPO}/releases/download"
     DOWNLOAD_URL="${DOWNLOAD_BASE}/${LATEST_RELEASE_TAG}/${FLOWCTL_ARTIFACT}"
 
-    if [ "$HTTP_REQUEST_FLOWCTL" == "curl" ]; then
+    if [[ "$HTTP_REQUEST_FLOWCTL" == "curl" ]]; then
         httpstatus=$(curl -sSLI -o /dev/null -w "%{http_code}" "$DOWNLOAD_URL")
-        if [ "$httpstatus" == "200" ]; then
+        if [[ "$httpstatus" == "200" ]]; then
             return 0
         fi
     else
         wget -q --spider "$DOWNLOAD_URL"
         exitstatus=$?
-        if [ $exitstatus -eq 0 ]; then
+        if [[ $exitstatus -eq 0 ]]; then
             return 0
         fi
     fi
@@ -158,19 +158,19 @@ installFile() {
     tar xf "$ARTIFACT_TMP_FILE" -C "$FLOWCTL_TMP_ROOT"
     local tmp_root_flowctl="$FLOWCTL_TMP_ROOT/$FLOWCTL_FILENAME"
 
-    if [ ! -f "$tmp_root_flowctl" ]; then
+    if [[ ! -f "$tmp_root_flowctl" ]]; then
         echo "Failed to unpack FlowCtl executable."
         exit 1
     fi
 
-    if [ -f "$FLOWCTL_FILE" ]; then
+    if [[ -f "$FLOWCTL_FILE" ]]; then
         runAsRoot rm "$FLOWCTL_FILE"
     fi
     chmod o+x $tmp_root_flowctl
     mkdir -p $FLOWCTL_INSTALL_DIR
     runAsRoot cp "$tmp_root_flowctl" "$FLOWCTL_INSTALL_DIR"
 
-    if [ -f "$FLOWCTL_FILE" ]; then
+    if [[ -f "$FLOWCTL_FILE" ]]; then
         echo "$FLOWCTL_FILENAME installed into $FLOWCTL_INSTALL_DIR successfully."
 
         $FLOWCTL_FILE version
@@ -182,7 +182,7 @@ installFile() {
 
 fail_trap() {
     result=$?
-    if [ "$result" != "0" ]; then
+    if [[ "$result" != "0" ]]; then
         echo "Failed to install FlowCtl"
         echo "For support, go to https://flowsynx.io"
     fi
@@ -208,7 +208,7 @@ trap "fail_trap" EXIT
 getSystemInfo
 checkHttpRequestFlowCtl
 
-if [ -z "$1" ]; then
+if [[ -z "$1" ]]; then
     echo "Getting the latest FlowCtl..."
     getLatestRelease
 else
